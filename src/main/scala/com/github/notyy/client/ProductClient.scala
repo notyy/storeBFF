224821480c25ba76c63d104dc4b6b429607553af
notyy <notyycn@gmail.com>
2016-12-01 22:03:40 +0800

    fix a bug of using a fixed "router" parameter

--- src/main/scala/com/github/notyy/client/ProductClient.scala
+++ src/main/scala/com/github/notyy/client/ProductClient.scala
@@ -28,15 +28,11 @@ trait ProductClient extends ProductSerializer {
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
   def queryProductByName(name: String): Future[Option[Product]] = {
-    val theUri = Uri(path = Path("/product/")).withQuery(Query(Map("name" -> "router")))
-    logger.info(s"query string is ${theUri.queryString()}")
-    logger.info(s"uri is $theUri")
+    val theUri = Uri(path = Path("/product/")).withQuery(Query(Map("name" -> s"$name")))
     val source = Source.single(HttpRequest(uri = theUri))
     val flow = Http().outgoingConnection(host, port).mapAsync(1) { rs =>
-      logger.info(s"get resonse, status code is ${rs.status}")
       rs.status match {
         case StatusCodes.OK => {
-          logger.info(s"response ok, entity is ${rs.entity}")
           Unmarshal(rs.entity).to[Product].map(Some(_))
         }
         case StatusCodes.NotFound => Future(None)
