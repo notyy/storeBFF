fba4c0bb1a38c59e419fdabfb63532780ae6fe09
notyy <notyycn@gmail.com>
2016-12-01 22:03:29 +0800

    add a new pact test for case of product not found

--- src/test/scala/com/github/notyy/contractTest/ProductServiceContractSpec.scala
+++ src/test/scala/com/github/notyy/contractTest/ProductServiceContractSpec.scala
@@ -16,33 +16,58 @@ class ProductServiceContractSpec extends FunSpec with ShouldMatchers {
   implicit val formats = DefaultFormats
 
   describe("ProductService provider") {
-    it("should be able to find product by name") {
-      forgePact
-        .between("storeBFF")
-        .and("storeProduct")
-        .addInteraction(
-          interaction
-            .description("find product by name")
-            .given("product named `router` exists")
-            .uponReceiving("/product/?name=router")
-            .willRespondWith(
-              status = 200,
-              headers = Map("Content-Type" -> "application/json"),
-              body = write(Product("1", "router")))
-        )
-        .runConsumerTest { mockConfig =>
-          val productClient = new ProductClient {
-            protected override def host: String = mockConfig.host
+    describe("shold find product by name") {
+      it("should be able to find product by name if exist") {
+        forgePact
+          .between("storeBFF")
+          .and("storeProduct")
+          .addInteraction(
+            interaction
+              .description("find product if exist")
+              .given("product named `router` exists")
+              .uponReceiving("/product/?name=router")
+              .willRespondWith(
+                status = 200,
+                headers = Map("Content-Type" -> "application/json"),
+                body = write(Product("1", "router")))
+          )
+          .runConsumerTest { mockConfig =>
+            val productClient = new ProductClient {
+              protected override def host: String = mockConfig.host
 
-            protected override def port: Int = mockConfig.port
+              protected override def port: Int = mockConfig.port
+            }
+
+            val optProduct = Await.result(productClient.queryProductByName("router"), 1 second)
+            optProduct should not be empty
+            optProduct.get.id should not be empty
+            optProduct.get.name shouldBe "router"
           }
+      }
+      it("returns 404 if product not found") {
+        forgePact
+          .between("storeBFF")
+          .and("storeProduct")
+          .addInteraction(
+            interaction
+              .description("returns 404 if product not found")
+              .given("product named `not_exist` doesn't exists")
+              .uponReceiving("/product/?name=not_exist")
+              .willRespondWith(
+                status = 404
+              )
+          )
+          .runConsumerTest { mockConfig =>
+            val productClient = new ProductClient {
+              protected override def host: String = mockConfig.host
 
-          val optProduct = Await.result(productClient.queryProductByName("router"), 1 second)
-          optProduct should not be empty
-          optProduct.get.id should not be empty
-          optProduct.get.name shouldBe "router"
-        }
+              protected override def port: Int = mockConfig.port
+            }
+
+            val optProduct = Await.result(productClient.queryProductByName("not_exist"), 1 second)
+            optProduct shouldBe empty
+          }
+      }
     }
   }
-
 }
